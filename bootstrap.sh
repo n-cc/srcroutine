e361dbd4cb3ff5ab7e1296d0083b5fcd4633e4eb
n-cc <nathanial.c.collins@gmail.com>
2019-02-10 19:35:08 -0600

    bootstrap.sh initialization

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+###############################
+#
+#  THIS SCRIPT IS UNTESTED
+#
+###############################
+SRCROUTINE_ROOT="/usr/src"
+LFS_SOURCES="/sources"
+MOVE_LFS=1 # whether or not to organize /sources dir into SRCROUTINE_ROOT; assumes kernel sources and patches are in LFS_SOURCES like LFS documents
+BIN_DIR="/usr/local/bin"
+
+# generate file structures
+mkdir -p $SRCROUTINE_ROOT/{lib,tars/linux,patches/lfs,routines/{lfs,linux}}
+
+# if enabled, convert LFS structure to srcroutine structure 
+if [[ "$MOVE_LFS" == 1 ]]; then
+  mv $LFS_SOURCES $SRCROUTINE_ROOT/tars/lfs
+  mv $SRCROUTINE_ROOT/tars/lfs/linux* $SRCROUTINE_ROOT/tars/linux/
+  mv $SRCROUTINE_ROOT/tars/lfs/*.patch $SRCROUTINE_ROOT/patches/lfs
+fi
+
+# copy over scripts
+cp ./bin/srcroutine $BIN_DIR/bin
+cp ./lib/env $SRCROUTINE_ROOT/lib/env
