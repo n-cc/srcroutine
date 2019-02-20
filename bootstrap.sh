db959227294351d6480984e84024e63781575c53
n-cc <nathanial.c.collins@gmail.com>
2019-02-19 19:04:59 -0600

    add sanity

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,9 +14,9 @@ mkdir -p $SRCROUTINE_ROOT/{lib,tars/linux,patches/lfs,routines/{lfs,linux}}
 
 # if enabled, convert LFS structure to srcroutine structure 
 if [[ "$MOVE_LFS" == 1 ]]; then
-  mv $LFS_SOURCES $SRCROUTINE_ROOT/tars/lfs
-  mv $SRCROUTINE_ROOT/tars/lfs/linux* $SRCROUTINE_ROOT/tars/linux/
-  mv $SRCROUTINE_ROOT/tars/lfs/*.patch $SRCROUTINE_ROOT/patches/lfs
+	mv $LFS_SOURCES $SRCROUTINE_ROOT/tars/lfs
+	mv $SRCROUTINE_ROOT/tars/lfs/linux* $SRCROUTINE_ROOT/tars/linux/
+	mv $SRCROUTINE_ROOT/tars/lfs/*.patch $SRCROUTINE_ROOT/patches/lfs
 fi
 
 # copy over scripts
@@ -24,4 +24,5 @@ cp ./bin/srcroutine $BIN_DIR/bin
 cp ./lib/env $SRCROUTINE_ROOT/lib/env
 
 # replace placeholders with vars from headers
-sed -i s/BOOSTRAP_SRCROUTINE_ROOT_PLACEHOLDER/$SRCROUTINE_ROOT/ /lib/env
+sed -i s/BOOSTRAP_SRCROUTINE_ROOT_PLACEHOLDER/$SRCROUTINE_ROOT/g $SRCROUTINE_ROOT/lib/env
+sed -i s/BOOSTRAP_SRCROUTINE_ROOT_PLACEHOLDER/$SRCROUTINE_ROOT/g $BIN_DIR/srcroutine
