58c6804fe43c651050be354bbde869c727a5945f
n-cc <nathanial.c.collins@gmail.com>
2019-02-19 19:30:20 -0600

    more sanity

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-###############################
-#
-#  THIS SCRIPT IS UNTESTED
-#
-###############################
+
+# root for all srcroutine files
 SRCROUTINE_ROOT="/usr/src"
+
+# where the LFS sources currently live (see below)
 LFS_SOURCES="/sources"
-MOVE_LFS=1 # whether or not to organize /sources dir into SRCROUTINE_ROOT; assumes kernel sources and patches are in LFS_SOURCES like LFS documents
+
+# whether or not to organize /sources dir into SRCROUTINE_ROOT; assumes kernel sources and patches are in LFS_SOURCES like LFS documents
+MOVE_LFS=1
+
+# binary dir to install the srcroutine script
 BIN_DIR="/usr/local/bin"
+
+# whether or not to attempt to install as non-root user
+FORCE=0
+
+# begin bootstrapping code
+
+if [[ "$EUID" != "0" ]] && [[ "$FORCE" != 1 ]]; then
+	echo "bootstrap script must be run as root! to ignore this, modify the bootstrap.sh to set FORCE=1"
+	exit 1
+fi
+echo "before running this script, be sure you read the variables in the header and modified if nescessary! press any key to continue, CTRL^C to exit."
+read
 
 # generate file structures
 mkdir -p $SRCROUTINE_ROOT/{lib,tars/linux,patches/lfs,routines/{lfs,linux}}
